c27a63ba01fd1f5706306b96ec3499f733e8a6de
MANNEPALLI PRAVEENA <praveena.mannepalli03@gmail.com>
2024-05-26 19:30:00 +0530

    Add files via upload

---
+++ Project/SnakeAndLadder-LLD/Java files/Jump.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Jump {
+  private int initPos;
+  private int finalPos;
+  public Jump(int initPos,int finalPos) {
+    this.initPos = initPos;
+    this.finalPos = finalPos;
+  }
+  public int getFinalPos () {
+    return finalPos;
+  }
+}
+
+
+
